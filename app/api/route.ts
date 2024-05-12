c8b304bba56d930f366d88109dc5b06a4c110b1e
Mubsal <musa2100@student.miun.se>
2024-05-12 04:04:58 +0200

    ändrade till export funktion till gemen

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,9 +1,9 @@
 import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
 import { authOptions } from "@/lib/auth";
-import {getSession} from "next-auth/react";
+//import {getSession} from "next-auth/react";
 
-export async function Get(req, res) {
+export async function get(req, res) {
   const session = await getServerSession(req, authOptions);
 
   return NextResponse.json({ authenticated: !!session });
