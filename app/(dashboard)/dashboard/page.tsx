528270a4df5da2c7c8ecb93ebdbfe4424100b746
Mubsal <musa2100@student.miun.se>
2024-05-14 01:00:20 +0200

    .

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -21,8 +21,7 @@ export default function page() {
       <div className="flex-1 space-y-4 p-4 md:p-8 pt-6">
         <div className="flex items-center justify-between space-y-2">
           <h2 className="text-3xl font-bold tracking-tight">
-            Hej älskade, bästa, finaste fru!
-            Tack för ditt tålamod! 👋
+            Hi, Welcome back 👋
           </h2>
           <div className="hidden md:flex items-center space-x-2">
             <CalendarDateRangePicker />
