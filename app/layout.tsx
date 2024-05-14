afe3110c69934b32653415194723d04b335cd0dd
Mubsal <musa2100@student.miun.se>
2024-05-14 02:21:44 +0200

    Installerat vercel web analytic

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { getServerSession } from "next-auth";
+import { Analytics } from "@vercel/analytics/react";
 
 
 const inter = Inter({ subsets: ["latin"] });
